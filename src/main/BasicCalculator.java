d93612a8928d4c68073f9acc34a80c4472b12339
Cy Lim <cyberdoomer@gmail.com>
2015-06-15 02:20:47 +0800

    Revert "update"
    This reverts commit 48f6a1bd60842e412af62f1d1660b1677f44edb1.

--- src/main/BasicCalculator.java
+++ src/main/BasicCalculator.java
@@ -4,7 +4,7 @@ public class BasicCalculator {
 	public static int add(int a, int b){
 		return a+b;
 	}
-	publici static int minus(int a, int b){
+	public static int minus(int a, int b){
 		return b-a;
 	}
 }
